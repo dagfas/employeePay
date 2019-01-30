51ee8a16d505d4f395f4c8fbaf9b403910367161
dagfas <dougmc134@yahoo.com>
2019-01-30 11:03:47 -0500

    Updated overtime pay

--- src/employeePay.java
+++ src/employeePay.java
@@ -13,13 +13,21 @@ public class employeePay {
 
         int totalHoursWorked = hoursWorked;
 
+        int hourlyMaximumPay = 25;
+        int paycheckWithoutOT = totalHoursWorked * hourlyMaximumPay;
         int overtimeWorked = totalHoursWorked - 35;
+        Double overtimePay = overtimeWorked * (hourlyMaximumPay * 1.5);
+        Double totalPay = paycheckWithoutOT + overtimePay;
 
-        if (totalHoursWorked > 35 && totalHoursWorked <60){
-            System.out.println("Overtime pay for " + employee1 + " is " + overtimeWorked * 1.5);
-            System.out.println();
+
+        if (totalHoursWorked <= 35){
+            System.out.println(employee1 + ", your weekly total take home is " + paycheckWithoutOT);
+        }
+        else if (totalHoursWorked > 35 && totalHoursWorked <60){
+            System.out.println("Overtime pay for " + employee1 + " is " + overtimePay);
+            System.out.println(employee1 + ", your weekly total take home is " + totalPay );
         }else{
-            System.out.println(employee1 + ", you cannot go beyond 60 hours per week");
+            System.out.println(employee1 + ", sorry you cannot go beyond 60 hours per week");
         }
 
 
